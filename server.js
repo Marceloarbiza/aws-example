879db169d4269b4b27a04545a690ff6bce1946b2
marceloarbiza <arbizamar@gmail.com>
2023-08-15 19:25:02 -0300

    port update

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.get('/', (req, res) => {
     }
 );
 
-app.listen(3000, () => {
-    console.log('Example app listening on port 3000!');
+app.listen(8080, () => {
+    console.log('Example app listening on port 8080!');
     }
 );
